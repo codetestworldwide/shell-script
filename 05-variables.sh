910cb1a667092f8a4cd906121d72542eaa55cc75
codetestworldwide <khyma9300@gmail.com>
2025-01-02 01:21:05 -0500

    05 script added

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter your username::" # text entered here will be added as value to variable
+
+read USERNAME # here USERNAME is variable name
+
+echo "username entered :$USERNAME
