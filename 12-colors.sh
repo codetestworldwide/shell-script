084ef8b53914846805af3f6a39e0471d077e55e2
codetestworldwide <khyma9300@gmail.com>
2025-01-03 16:11:23 -0500

    script added

---
+++ 12-colors.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+       echo "$2 ... $R FAILURE"
+       exit 1
+  else
+       echo "$2 ... $G SUCCESS"
+  fi
+}    
+ 
+if [ $USERID  -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ] 
+then # not installed
+   dnf install mysql -y
+   VALIDATE $? "Installing Mysql"
+else
+   echo -e "Mysql is already ...$Y INSTALLED"
+fi
+
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+     dnf install git -y
+     VALIDATE $?"Installing Git"
+else
+    echo "Git is already ... $Y INSTALLED"
+fi
+    
+
+
+
+
+
+
+
+
+
+    
