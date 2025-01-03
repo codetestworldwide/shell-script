259b78413d33ad3842483a7c80cb7b9a61096dbd
codetestworldwide <khyma9300@gmail.com>
2025-01-02 23:38:47 -0500

    05 script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,6 +4,6 @@ echo "please enter your username::" # text entered here will be added as value t
 
 read -s USERNAME # here USERNAME is variable name
 
-echo "username entered :$USERNAME
+echo "username entered :$USERNAME"
 
 echo "please enter your password: :"
