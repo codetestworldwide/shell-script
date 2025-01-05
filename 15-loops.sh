c1c040eb937c792f48ce32f324c087b31ebb456d
codetestworldwide <khyma9300@gmail.com>
2025-01-05 02:17:35 -0500

    script added

--- 15-loops.sh
+++ 15-loops.sh
@@ -32,11 +32,11 @@ fi
 for package in $@
 do 
   dnf list installed $package &>>$LOG_FILE_NAME
-  if [$? -ne 0 ]
+  if [ $? -ne 0 ]
   then 
-    dnf install $package -y &>>$LOG_FILE_NAME
-    VALIDATE $? "Installing $package"
+      dnf install $package -y &>>$LOG_FILE_NAME
+      VALIDATE $? "Installing $package"
   else
-     echo -e "$package is already $Y"
+     echo -e "$package is already $Y" ... INSTALLED $N"
    fi    
 done
